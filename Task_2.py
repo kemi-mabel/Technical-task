0e0cda1c3e6cf8ab4e783b0aa2d620afca90ed9e
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-04 06:32:26 -0700

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,16 @@
+"Write a function that accepts a positive integer and determines if it is a prime number."
+"The function should return true, if it is a prime number or false if it isn’t.)"
+
+pos_num = int(input("Enter a number"))
+
+
+def task2(pos_num):
+    for i in range(2, pos_num):
+        if pos_num % i == 0:
+            print("False")
+            break
+    else:
+        print("True")
+
+
+task2(pos_num)
