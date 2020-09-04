0434afd34f43cee1d63c29d16150372cd2663bea
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-04 06:32:37 -0700

    Create Task_5.py

---
+++ Task_5.py
@@ -0,0 +1,18 @@
+"""Write a method to replace all spaces in a string with '%20'.
+Example
+Input: "Mr John Smith "
+Output: "Mr%20John%20Smith"""
+
+class Replace:
+    def string():
+        sentence = input("Enter the string:")
+        sentence = sentence.replace(" ", "%20")
+        print(sentence)
+
+
+Replace.string()
+
+
+
+
+
