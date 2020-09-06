3085f0b8a343708dfa6ee5e6e0a7bb43b1276bfe
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:10:16 -0700

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -4,21 +4,21 @@ and returns an array of all prime numbers from the given array. ."""
 
 def task3():
     number_list = []
-    n = int(input("Enter the list size : "))
-
+    n = int(input("Enter the list size : "))  # enter how many number in array and covert to int
+    # adding each number in array to number_list
     for i in range(0, n):
         print("Enter number at index", i, ":")
         number = int(input())
         number_list.append(number)
 
-    prime_numbers = []
+    prime_numbers = []  # list of prime numbers
 
-    for e_number in number_list:  # for each number in the list
+    for e_number in number_list:  # for each number in number_list
         for i in range(2, e_number):  # for each number between the range of 2 to that number
-            if e_number % i == 0:  # if number/2......  = 0 stop the loop
+            if e_number % i == 0:  # if number/2 to e_number  = 0 stop the loop
                 break
         else:
-            prime_numbers.append(e_number) # add the number to prime number list
+            prime_numbers.append(e_number) # adding the number to prime number list
     print(prime_numbers)
 
 
