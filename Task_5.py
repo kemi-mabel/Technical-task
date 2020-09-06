0a813deb5d609872393ca9ab129668828f230ada
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:16:58 -0700

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -3,10 +3,10 @@ Example
 Input: "Mr John Smith "
 Output: "Mr%20John%20Smith"""
 
-class Replace:
-    def string():
+class Replace:  # defining the class
+    def string():  # defining the methid
         sentence = input("Enter the string:")
-        sentence = sentence.replace(" ", "%20")
+        sentence = sentence.replace(" ", "%20")  # using the replace method, replace the spaces with %20
         print(sentence)
 
 
