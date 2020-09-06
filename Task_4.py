65af055509bba0b1745c18ccd8627bee031334a3
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:16:55 -0700

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -6,25 +6,25 @@ and returns an integer value you can evaluate to determine the password strength
 2 -> strong e.g. a password containing strings and numbers
 3 -> very strong e.g. a password containing strings, numbers and special characters (!,@,#,$,%, etc)"""
 
-# from getpass import getpass
 from string import punctuation
 
 
 def password_validator():
     password = input("Enter your new password : ")
-    # p_word = getpass("Enter your new password : ")
-    alpha = password.isalpha()
+    alpha = password.isalpha()  # returns true if a string only contains letters
     alpha1 = any(x.isalpha() for x in password)
-    num = password.isnumeric()
-    num1 = any(x.isnumeric() for x in password)
-    alpha_num = password.isalnum()
-    # alpha_num1 = any(x.isalnum() for x in password)
+    num = password.isnumeric()  # returns true if a string only contains numbers
+    num1 = any(x.isnumeric() for x in password)  # returns true if any of the charcater in password is alpha
+    alpha_num = password.isalnum()  # returns true if a string only contains alphbets and numbers
     spec_char = ""
     validator = 0
+    # checking for characters
     for i in password:
         if i in punctuation:
             spec_char = True
             break
+
+    # scoring the passwords using validators
     if spec_char and alpha1 and num1:
         validator = 3
     elif alpha1 and spec_char:
