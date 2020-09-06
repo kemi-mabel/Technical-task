25ec448c56085f6b6637c9518b73497a95f9a3e8
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:10:12 -0700

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -4,12 +4,12 @@
 
 
 
-def task2(pos_num):
-    pos_num = int(input("Enter a number"))
-    for i in range(2, pos_num):
-        if pos_num % i == 0:
+def task2(pos_num):  # defining a function
+    pos_num = int(input("Enter a number"))  # convert input to an integar val
+    for i in range(2, pos_num):  # for each number from 2 to input
+        if pos_num % i == 0:  # if input mod (2 to (input-1)) = 0, false
             print("False")
-            break
+            break  # break once condition is true
     else:
         print("True")
 
