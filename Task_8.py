26c630ceb4ed91c8aea1548647e1fb09fd0fa4bf
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:17:06 -0700

    Update Task_8.py

--- Task_8.py
+++ Task_8.py
@@ -10,13 +10,13 @@ class Counter3:
     def count3(self):
         count = 0  # number of 3
         numbers_3 = []  # numbers with 3
-        all_numbers = []  # list of all numbers from 0 to self
-        for number in range(0, self + 1):  # 
-            all_numbers.append(str(number))
-        for number in all_numbers:
-            if "3" in number:
-                numbers_3.append(number)
-                count += number.count('3')
+        all_numbers = []  # list of all numbers from 0 to self + 1
+        for number in range(0, self + 1):  # for each number for 0 to self + 1
+            all_numbers.append(str(number))  # convert number to string and add to list
+        for number in all_numbers:  # for each number in list
+            if "3" in number:  # checking if 3 is in each number
+                numbers_3.append(number)  # adding the number to number_3
+                count += number.count('3') # count the number of 3 in that number and add to the previous count
 
         print("count: {} numbers:{}".format(count, numbers_3))
 
