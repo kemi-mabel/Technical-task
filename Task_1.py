de6292e7a4db7572d6cd2228723289bafb8f16e9
Oluwakemi <65091123+kemi-mabel@users.noreply.github.com>
2020-09-06 13:10:09 -0700

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -5,14 +5,14 @@ The first index in the returned array should hold the sum of the even integers
 and the second index should hold the sum of the odd integers."""
 
 
-def task1(num):
-    even_sum = 0
-    odd_sum = 0
+def task1(num):  # defining the function
+    even_sum = 0  # sum of even numbers
+    odd_sum = 0  # sum of odd numbers
     for i in num:  # for each nunmer in array
-        if i % 2 == 0:  # comparing i modulus to 0
-            even_sum += i
+        if i % 2 == 0:  # checking if it's an even or odd number using modulus
+            even_sum += i  # if even add to even_sum
         else:
-            odd_sum += i
+            odd_sum += i  #if odd add to odd_sum
     return [even_sum, odd_sum]
 
 
